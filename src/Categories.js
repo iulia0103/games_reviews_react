0cc2478cb4cbfe9355738c2666d3dff1b3fe06a5
iulia0103 <iulia.leancu@gmail.com>
2016-04-28 16:49:26 +0200

    fghjsfv

--- src/Categories.js
+++ src/Categories.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import jQuery from 'jquery';
+import {Link} from 'react-router';
+
 
 class Categories extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: []
+    };
+  }
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  getCategories() {
+    let component = this;
+    let url = "https://hidden-springs-35080.herokuapp.com/categories.json";
+    jQuery.getJSON(url, function(data){
+      component.setState({
+        categories: data.categories
+      });
+    });
+  }
+
   render() {
     return(
-      <h1>Categories</h1>
+      <div className="categories">
+        <h1>Categories</h1>
+
+        <ul>
+          {this.state.categories.map(function(category) {
+            return(
+              <li key={category.id}>
+                <Link to={`/categories/${category.id}`}>{category.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
     );
   }
 }
